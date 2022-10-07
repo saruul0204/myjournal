39010313a99715267f83781ab002544ff276e099
saruul0204 <saruul0204@yahoo.com>
2022-10-07 22:12:59 +0200

    Design for home page and postindex, show, form

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_233504) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_195729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
