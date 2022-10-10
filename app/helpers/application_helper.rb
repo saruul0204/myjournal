0ff140b3e597ca9ae326bb39b80f37691dce4505
saruul0204 <saruul0204@yahoo.com>
2022-10-10 18:29:16 +0200

    Implement pagination for posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-    def field_has_error(field_name, form_object)
-      return unless form_object.errors.include?(field_name)
+  include Pagy::Frontend
 
-      error_tag = form_object.errors.full_messages_for(field_name).join('. ')
-      content_tag(:p, error_tag, class: 'error-message')
-    end
+  def field_has_error(field_name, form_object)
+    return unless form_object.errors.include?(field_name)
+
+    error_tag = form_object.errors.full_messages_for(field_name).join('. ')
+    content_tag(:p, error_tag, class: 'error-message')
+  end
 end
